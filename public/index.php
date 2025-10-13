345e9395c398162e3c4ff7baa66dcf1042a855fa
William <euwilliamsantana@gmail.com>
2025-10-13 19:58:00 -0300

    Create Routes and Validation

--- public/index.php
+++ public/index.php
@@ -9,3 +9,32 @@
  * Iremos definir as rotas
   */
 
+ // Init Session
+ session_start();
+
+ //Constante de controle
+ define("CONTROL", ture);
+
+ // Verificamos se existe um user logado
+ $usuario_logado = $_SESSION["usuario"] ?? null;
+
+ // Verificamos qual é a rota na URL
+ if(empty($usuario_logado)){
+  $rota = "login";
+ }else{
+  $rota = $_GET["rota"] ?? "home";
+ }
+
+ // Analisar a rota
+ $rotas = [
+  "login" => "login.php"
+  "home" => "home.php"
+ ]
+
+ // Verificando e acessando a rota
+ if(!key_exists($rota, $rotas)){
+  die("Acesso negado!")
+ }
+
+  require_once $rotas[$rota]
+ 
